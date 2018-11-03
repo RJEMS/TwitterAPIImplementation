fed2aa9ce44d3ab7ae6c3964e134c597a93ca87c
Emmeline Tsen <etsen@ucdavis.edu>
2018-11-03 15:12:51 -0700

    Updated include_entities within SearchTweets.py and UsersLookup.py

--- UsersLookup.py
+++ UsersLookup.py
@@ -12,6 +12,8 @@ def buildlookup(formdata):
         querydata = querydata + '&screen_name=' + formdata['parameters_screen_name']
     if formdata['parameters_user_id'] != "":
         querydata = querydata + '&user_id=' + formdata['parameters_user_id']
+    if formdata['parameters_include_entities'] != "":
+        querydata = querydata + '&include_entities=' + formdata['parameters_include_entities']
 
     print (querydata)
     return oauth_req('https://api.twitter.com/1.1/users/lookup.json' + querydata)
